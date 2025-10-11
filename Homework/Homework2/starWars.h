f074aaedf3928217bd000a998fdd331d6a68cf6b
Trevor Starr <trevorstarr@MBP14inch2021.localdomain>
2025-10-10 22:04:54 -0600

    Header file mods

--- Homework/Homework2/starWars.h
+++ Homework/Homework2/starWars.h
@@ -4,22 +4,30 @@
 #include <string>
 
 namespace starwars {
-    class Jedi{
-        private:
+    class Character {
+        protected:
             std::string name;
             int health;
+            int attackPower;
+        public:
+            Character(std::string n = "Unknown", int h = 100, int ap = 50);
+            virtual ~Character() = default;
+            std::string getName() const;
+            int getHealth() const;
+            virtual void takeDamage(int damage);
+            virtual int attack() const;
+    };
+
+    class Jedi{
+        protected:
             int lightsaberSkill;
             int forcePower;
 
         public:
             Jedi(std::string n = "Unknown Jedi", int h = 100, int ls = 50, int fp = 75);
 
-
-            std::string getName() const;
-            int getHealth() const;
             int getLightsaberSkill() const;
             int getForcePower() const;
-
             void setName(std::string n);
             void setHealth(int h);
             void setLightsaberSkill(int ls);
@@ -27,28 +35,35 @@ namespace starwars {
 
             int attack() const;
             void useForce(int power);
-            void takeDamage(int damager);
+            void takeDamage(int damage);
 
             void saveToFile(const std::string& filename) const;
             void loadFromFile(const std::string& filename);
     };
 
-    class Sith {
-        private:
-            std::string name;
-            int health;
-            int lightsaberSkill;
-
-
+    class Guardian : public Jedi {
         public:
-            Sith(std::string n = "Unknown Sith", int h = 120, int ls = 60);
+            Guardian(std::string n = "Guardian Jedi");
+    };
+    class Consular : public Jedi {
+        public:
+            Consular(std::string n = "Consular Jedi");
+    };
 
-            std::string getName() const;
-            int getHealth() const;
-            int getLightsaberSkill() const;
-
-            void takeDamage(int damager);
+    class Sith {
+        public:
+            Sith(std::string n = "Unknown Sith", int h = 120, int ap = 60);
+            void takeDmaage(int damage);
             int attack() const;
+    };
+
+    class Acolyte : public Sith {
+        public:
+            Acolyte(std::string n = "Acolyte Sith");
+    };
+    class Darth : public Sith {
+        public:
+            Darth(std::string n = "Darth Sith");
     };
 }
 
