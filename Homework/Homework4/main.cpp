ccf8a954171c8b06958cb719b2488e49714c641f
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-11-28 20:51:01 -0700

    main.cpp: bulk of evaluateRPN

--- Homework/Homework4/main.cpp
+++ Homework/Homework4/main.cpp
@@ -7,7 +7,37 @@ using namespace std;
 
 double evaluateRPN(string expression)
 {
-    return 0.0;
+    Stack s;
+    stringstream ss(expression);
+    string token;
+
+    while(ss >> token){
+        if(isdigit(token[0]) || ((token[0] == '-' || token[0] == '+') && token.size() > 1)){
+            s.push(stod(token));
+        }
+        else if(token == "+" || token == "-" || token == "*" || token == "/"){
+            if(s.size() < 2){
+                cout << "Invalid expression." << endl;
+                return NAN;
+            }
+            
+            //for first op.
+            double b = s.pop();
+            //for second op
+            double a = s.pop();
+
+            if(token == "+")         s.push(a + b);
+            else if(token == "-")    s.push(a - b);
+            else if(token == "*")    s.push(a * b);
+            else if(token =="/"){
+                if(b == 0){
+                    cout << "Error: division by zero" << endl;
+                    return NAN;
+                }
+                s.push(a / b);
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[])
