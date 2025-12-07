09045a2c4f443a129ffaa3e102c7ca74602521db
Trevor Starr <trevorstarr@MBP14inch2021.localdomain>
2025-12-07 14:56:15 -0700

    room.cpp constructor and three starting void functions

--- Final/room.cpp
+++ Final/room.cpp
@@ -1,1 +1,12 @@
+#include "room.h"
 
+Room::Room(std::string desc){
+    description = desc;
+    enemy = nullptr;
+    item = nullptr;
+    next = nullptr;
+}
+
+void Room::setEnemy(Enemy* e) {enemy = e;}
+void Room::setItem(Item* i) {item = i;}
+void Room::setNext(Room* n) {next = n;}
