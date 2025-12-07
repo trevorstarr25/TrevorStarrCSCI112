477a54288cca0e0f4a0d79612321ebaf65a4f5fa
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-12-07 12:30:31 -0700

    dungeon.h (draft)

--- Final/dungeon.h
+++ Final/dungeon.h
@@ -1,1 +1,12 @@
+#pragma once
+#include "room.h"
 
+class Dungeon{
+    private:
+        Room* head;
+
+    public:
+        Dungeon();
+        void addRoom(Room* room);
+        Room* getHead();
+};
