a69959ce4b002bf7f9b16b605f6f6266930d9ca1
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-12-07 12:21:47 -0700

    item.cpp (draft)

--- Final/item.cpp
+++ Final/item.cpp
@@ -1,1 +1,18 @@
+#include "item.h"
 
+Item::Item(std::string n, int heal){
+    name = n;
+    healAmount = heal;
+}
+
+std::string Item::use(){
+    return name;
+}
+
+int Item::getHealAmount(){
+    return healAmount;
+}
+
+std::string Item::getName(){
+    return name;
+}
