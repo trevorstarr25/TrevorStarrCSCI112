d40f53c95aaf64a7f875e67cce813e7a8b9a704d
Trevor Starr <trevorstarr@MBP14inch2021.localdomain>
2025-12-07 15:31:18 -0700

    Fleshing out main.cpp game functions

--- Final/main.cpp
+++ Final/main.cpp
@@ -12,5 +12,19 @@ int main(){
     cin >> pname;
 
     Player player(pname);
+
+    Dungeon dungeon;
+
+    Room* r1 = new Room("You are in the entrance of a dark dungeon.");
+    Room* r2 = new Room("A narrow hallway lit by torchlight.");
+    Room* r3 = new Room("A room with a large chest. A troll guards it!");
+
+    r1->setEnemy(new Enemy("Goblin", 30, 5));
+    r2->setItem(new Item("Health Potion", 25));
+    r3->setEnemy(new Enemy("Dragon", 100, 20));
+
+    dungeon.addRoom(r1);
+    dungeon.addRoom(r2);
+    dungeon.addRoom(r3);
 }
 
