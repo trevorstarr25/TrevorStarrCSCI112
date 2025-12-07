444879f1911315b58e3a05eb2604b71a86e713c8
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-12-07 12:13:11 -0700

    item.h draft

--- Final/item.h
+++ Final/item.h
@@ -1,1 +1,15 @@
+#pragma once
+#include <string>
 
+class Item{
+    private:
+        std::string name;
+        int healAmount;
+
+    public:
+        Item(std::string n, int heal);
+
+        std::string use();
+        int getHealAmount();
+        std::string getName();
+};
