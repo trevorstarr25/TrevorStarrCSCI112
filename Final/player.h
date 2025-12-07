e50c0fe8ed30039169611094836a7052218c51ee
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-12-07 12:03:54 -0700

    Completed (draft) of player.h, room.h (incomplete)

--- Final/player.h
+++ Final/player.h
@@ -1,1 +1,21 @@
+#pragma once
+#include <string>
+#include <vector>
 
+class Player{
+    private:
+        std::string name;
+        int health;
+        std::vector<std::string> inventory;
+
+    public:
+        Player(std::string n);
+
+        void attack();
+        void takeDamage(int amount);
+        void addToInventory(std::string item);
+        void showInventory();
+
+        int getHealth();
+        std::string getName();
+};
--- Final/room.h
+++ Final/room.h
@@ -1,1 +1,18 @@
+#pragma once
+#include <string>
+#include "enemy.h"
+#include "item.h"
 
+class Room{
+    private:
+        std::string description;
+        Enemy* enemy;
+        Item* item;
+        Room* next;
+    
+    public:
+        Room(std::string desc);
+        void setEnemy(Enemy* e);
+        void setItem(Item* i);
+        void setNext(Room* n);
+}
