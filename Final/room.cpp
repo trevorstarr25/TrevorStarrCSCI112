56324e86834242fa35f6da0ff6cb7c814f819682
Trevor Starr <trevorstarr@MBP14inch2021.localdomain>
2025-12-07 15:00:14 -0700

    room.cpp remaining void and getters

--- Final/room.cpp
+++ Final/room.cpp
@@ -10,3 +10,12 @@ Room::Room(std::string desc){
 void Room::setEnemy(Enemy* e) {enemy = e;}
 void Room::setItem(Item* i) {item = i;}
 void Room::setNext(Room* n) {next = n;}
+
+Enemy* Room::getEnemy() {return enemy;}
+Item* Room::getItem() {return item;}
+Room* Room::getNext() {return next;}
+std::string Room::getDescription() {return description;}
+
+void Room::removeEnemy() {enemy = nullptr;}
+void Room::removeItem() {item = nullptr;}
+
