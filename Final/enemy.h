86a0e5a1862fc30dfa006c9f54fdd6f0b3d12946
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-12-07 12:08:38 -0700

    Draft of enemy.h

--- Final/enemy.h
+++ Final/enemy.h
@@ -1,1 +1,19 @@
+#pragma once
+#include <string>
 
+class Enemy{
+    private:
+        std::string name;
+        int health;
+        int damage;
+
+    public:
+        Enemy(std::string n, int h, int d);
+
+        int attack();
+        void takeDamage(int amount);
+
+        bool isDead();
+        std::string getName();
+        int getHealth();
+};
