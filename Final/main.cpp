e4310e2ab9e6dbf4f8271b0109991acbb3d93046
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-12-08 10:35:49 -0700

    player.cpp and building game in main

--- Final/main.cpp
+++ Final/main.cpp
@@ -17,14 +17,53 @@ int main(){
 
     Room* r1 = new Room("You are in the entrance of a dark dungeon.");
     Room* r2 = new Room("A narrow hallway lit by torchlight.");
-    Room* r3 = new Room("A room with a large chest. A troll guards it!");
+    Room* r3 = new Room("A room with a large chest. A dragon guards it!");
 
     r1->setEnemy(new Enemy("Goblin", 30, 5));
+    r1->setItem(new Item("Health Potion", 15));
+
     r2->setItem(new Item("Health Potion", 25));
+    r2->setEnemy(new Enemy("Orc", 50, 25));
+
     r3->setEnemy(new Enemy("Dragon", 100, 20));
+    r3->setItem(new Item("A chest full of gold", 0));
 
     dungeon.addRoom(r1);
     dungeon.addRoom(r2);
     dungeon.addRoom(r3);
+
+    Room* current = dungeon.getHead();
+
+    while(current != nullptr){
+        cout << "\n--- " << current->getDescription() << "\n---" << endl;
+
+        if(current->getEnemy()){
+            Enemy* e = current->getEnemy();
+            cout << "An enemy appears and blocks your path: " << e->getName() << " (HP: " << e->getHealth() << ")" << endl;
+
+            while(!e->isDead() && player.getHealth() > 0){
+                cout << "1. Attack\n2. Check Inventory" << endl;
+                int choice;
+                cin >> choice;
+
+                if(choice == 1){
+                    player.attack();
+                    e->takeDamage(20);
+
+                    if(!e->isDead()){
+                        std::cout << "\nThe " << e->getName() << " swings back and deals " << e->attack() << " damage!" << std::endl;
+                        player.takeDamage(e->attack());
+                    }
+                }
+            }
+
+            std::cout << e->getName() << " has been defeated!" << std::endl;
+            current->removeEnemy();
+        }
+
+        if(current->getItem()){
+            
+        }
+    }
 }
 
--- Final/player.cpp
+++ Final/player.cpp
@@ -1,1 +1,44 @@
+#include "player.h"
+#include <iostream>
 
+Player::Player(std::string n){
+    name = n;
+    health = 100;
+}
+
+void Player::attack(){
+    std::cout <<  "You attack the enemy!" << std::endl;
+}
+
+void Player::takeDamage(int amount){
+    health -= amount;
+    if(health < 0){
+        health = 0;
+        std::cout << "You took " << amount << " damage and have perished." << std::endl;
+    }
+    else{
+        std::cout << "You took " << amount << " damage. Health: " << health << std::endl;
+    }
+
+}
+
+void Player::addToInventory(std::string item){
+    inventory.push_back(item);
+    std::cout << item << " added to inventory." << std::endl;
+}
+
+void Player::showInventory(){
+    std::cout << "Inventory: ";
+    for(std::string i : inventory){
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+}
+
+int Player::getHealth(){
+    return health;
+}
+
+std::string Player::getName(){
+    return name;
+}
