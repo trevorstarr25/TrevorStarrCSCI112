970cf6944f5c4f851997f5ed88728d1930953c8d
Trevor Starr <trevorstarr@Trevors-MacBook-Air-2.local>
2025-12-10 11:51:07 -0700

    draft but probably final of sith.cpp

--- Homework/Homework 1/sith.cpp
+++ Homework/Homework 1/sith.cpp
@@ -1,1 +1,22 @@
+#include "sith.h"
 
+namespace starwars{
+    Sith::Sith(std::string n, int h, int ls) : name(n), health(h), lightsaberSkill(ls) {}
+
+    std::string Sith::getName() const {return name;}
+    int Sith::getHealth() const {return health;}
+    int Sith::getLightsaberSkill() const {return lightsaberSkill;}
+
+    void Sith::setName(const std::string& n) {name = n;}
+    void Sith::setHealth(int h) {health = h;}
+    void Sith::getLightsaberSkill(int ls) {lighsaberSkill = ls;}
+
+    int Sith::attack() const{
+        return lightsaberSkill;
+    }
+
+    void Sith::takeDamage(int damage){
+        health -= damage;
+        if(health < 0) health = 0;
+    }
+}
